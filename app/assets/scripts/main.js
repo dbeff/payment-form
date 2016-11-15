fccaf8edb3944e9e6d6b58a59656b4b0d1af0e36
Daniel Beff <daniel.beff@gmail.com>
2016-11-15 18:35:36 -0200

    refactor jsonp request and add some validations

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,17 +1,51 @@
-function parsePayload(payload) {
-    var resultElement = document.querySelector(".results");
-    resultElement.innerHTML = payload.toString();
+var form = document.getElementById("payment-form");
+var name = form.elements["country"];
+var country = form.elements["country"];
+var cost = form.elements["cost"];
+var city = form.elements["city"];
+var cardNumber = form.elements["card-number"];
+var results = document.querySelector(".results");
+var costs = {
+    "DE": "2,5€",
+    "AT": "3€",
+    "ES": "5,45€",
+    "FR": "2€",
+    "UK": "2,75GBP",
 }
 
-function ajax(filter, query) {
-    var script = document.createElement("script");
-    script.src = "//gd.geobytes.com/AutoCompleteCity?callback=parsePayload&filter=" + filter + "&q=" + query;
-    document.getElementsByTagName("head")[0].appendChild(script);
+function requestData(filter, query) {
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.getElementById("geobytes");
+    var url = "//gd.geobytes.com/AutoCompleteCity";
+    var params = "?callback=requestDataPayload&filter=" + filter + "&q=" + query;
+
+    if (script !== null) script.parentNode.removeChild( script );
+
+    script = document.createElement("script");
+    script.id = "geobytes";
+    script.src = url + params
+    head.appendChild(script);
+}
+
+function requestDataPayload(payload) {
+    results.innerHTML = payload.toString();
+}
+
+function onChangeCountry(e,value){
+    city.value = "";
+    cost.value = costs[country.value] || "";
+    results.innerHTML = "";
 }
 
 function onChangeCity(e, value) {
-    var country = document.querySelector(".country");
-    if (value.length >= 3 && country !== "") {
-        ajax(country.value, value);
+    if (value.length >= 3 && country.value !== "") {
+        requestData(country.value, value);
+    } else {
+        results.innerHTML = "";
     }
+}
+
+function validateForm(e){
+    console.log("Validation form");
+    return true;
 }
